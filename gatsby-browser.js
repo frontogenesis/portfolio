7079b51df3f2d02c7b7cbe8c0ddbf08a27ed6b2e
Ray Hawthorne <frontogenesis@gmail.com>
2021-10-02 22:14:51 -0400

    Install react-bootstrap and import its CSS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,11 +3,14 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers
 import "./src/normalize.css"
+// bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css"
 // custom CSS styles
 import "./src/style.css"
 import "./src/projects.css"
 import "./src/articles.css"
 import "./src/card.css"
+
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
